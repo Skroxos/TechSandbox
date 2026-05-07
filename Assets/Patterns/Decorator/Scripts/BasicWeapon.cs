cc6249834b6d916b958f976030d513921b513cc6
Adam <krciladam@seznam.cz>
2026-05-07 15:44:16 +0200

    Added Basic implementation of Decorator pattern

---
+++ Assets/Patterns/Decorator/Scripts/BasicWeapon.cs
@@ -0,0 +1,17 @@
+﻿namespace Patterns.Decorator.Scripts
+{
+    public class BasicWeapon : IWeapon
+    {
+        private int _damage;
+        
+        public BasicWeapon(int damage)
+        {
+            _damage = damage;
+        }
+
+        public int GetDamage()
+        {
+            return _damage;
+        }
+    }
+}
---
+++ Assets/Patterns/Decorator/Scripts/PoisonEnchantment.cs
@@ -0,0 +1,17 @@
+﻿namespace Patterns.Decorator.Scripts
+{
+    public class PoisonEnchantment : WeaponDecorator
+    {
+        private int _poisonDamage;
+
+        public PoisonEnchantment(IWeapon weapon, int poisonDamage) : base(weapon)
+        {
+            _poisonDamage = poisonDamage;
+        }
+
+        public override int GetDamage()
+        {
+            return base.GetDamage() + _poisonDamage;
+        }
+    }
+}
