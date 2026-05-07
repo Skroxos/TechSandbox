a9710fb6860eeb123b635ae4857459e5b500c8d5
Adam <krciladam@seznam.cz>
2026-05-07 18:01:21 +0200

    Factory pattern

---
+++ Assets/Patterns/Factory/Scripts/Skeleton/SkeletonFactory.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Patterns.Factory.Scripts.Skeleton
+{
+    [CreateAssetMenu(fileName = "SkeletonFactory", menuName = "Factory/SkeletonFactory")]
+    public class SkeletonFactory : GeneralFactory
+    {
+        public override IEnemy CreateEnemy()
+        {
+            return new Skeleton(config);
+        }
+    }
+}
---
+++ Assets/Patterns/Factory/Scripts/Spawner.cs
@@ -0,0 +1,16 @@
+﻿using Patterns.Factory.Scripts.Skeleton;
+using UnityEngine;
+
+namespace Patterns.Factory.Scripts
+{
+    public class Spawner : MonoBehaviour
+    {
+        [SerializeField] private GeneralFactory factory;
+        private void Start()
+        {
+            IEnemy enemy = factory.CreateEnemy();
+            enemy.Initialize();
+            enemy.Attack();
+        }
+    }
+}
